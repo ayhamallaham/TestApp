d5a280f543aab7c7b7d812b10bbe53b97db2943c
ayham.allaham <ayham.allaham@rierino.com>
2022-10-04 22:26:01 +0300

    Initial version of test generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/ayham/testapp/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.ayham.testapp.service.dto;
---
+++ src/main/java/com/ayham/testapp/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.ayham.testapp.web.rest.vm;
